34860e1e0f0106bc8916f4b60dd279e327f1a8f4
Nguyen Thi Men <menne25122005@gmail.com>
2026-08-06 15:51:27 +0700

    feat(course-service): CRUD course 3-layer + DTO

---
+++ course-service/src/main/java/vn/edu/crs/course_service/repository/CourseRepository.java
@@ -0,0 +1,6 @@
+package vn.edu.crs.course_service.repository;
+import vn.edu.crs.course_service.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    boolean existsByTenMonHocIgnoreCase(String tenMonHoc);
+}
